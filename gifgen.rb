32532a8e838ff785fe70b8c54497bd09b7111b61
Luke Childs <lukechilds123@gmail.com>
2016-07-10 13:48:43 +0100

    Update gifgen to 1.1.0

--- gifgen.rb
+++ gifgen.rb
@@ -1,9 +1,9 @@
 class Gifgen < Formula
   desc "Simple high quality GIF encoding"
   homepage "https://github.com/lukechilds/gifgen"
-  url "https://github.com/lukechilds/gifgen/archive/1.0.1.zip"
-  version "1.0.1"
-  sha256 "958b9a50edbbcd310114a1e997a49c8a819b242e83e89a1cec93bb50e032017c"
+  url "https://github.com/lukechilds/gifgen/archive/1.1.0.zip"
+  version "1.1.0"
+  sha256 "29c8694b7a639fa0f75f6825d91149229217e54f80286666f5c03369f7c4f102"
 
   depends_on "ffmpeg"
 
