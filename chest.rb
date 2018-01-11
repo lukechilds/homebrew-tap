fc103c0003a3a5a931a3bddf82e7cf7b352be403
Luke Childs <lukechilds123@gmail.com>
2018-01-11 11:33:50 +0700

    Add gpg dependency to chest

--- chest.rb
+++ chest.rb
@@ -5,6 +5,8 @@ class Chest < Formula
   version "0.2.0"
   sha256 "28ba1d3ea118cb8805b9024fcfd4289e7945df90de99aa7ef81c17ea936a08f7"
 
+  depends_on "gpg"
+
   def install
     bin.install "chest"
   end
