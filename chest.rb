1a1d72668f896167b528504d70158f1da7a835cb
Luke Childs <lukechilds123@gmail.com>
2018-01-11 14:57:46 +0700

    Update chest to 0.4.0

--- chest.rb
+++ chest.rb
@@ -1,9 +1,9 @@
 class Chest < Formula
   desc "Bash glue to encrypt and hide files"
   homepage "https://github.com/lukechilds/chest"
-  url "https://github.com/lukechilds/chest/archive/0.3.0.zip"
-  version "0.3.0"
-  sha256 "a9342c5d18a9760f846979acfd2791d5815a8ae2bb71781519561fc2de9ff717"
+  url "https://github.com/lukechilds/chest/archive/0.4.0.zip"
+  version "0.4.0"
+  sha256 "40f9323acb0dd87f173b344364584f9531bf99c487f3fc1f2d6e6a9deacdd4be"
 
   depends_on "gpg"
 
