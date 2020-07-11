5636d1daca85e87a7c8bc249256e7d923961fc5e
Luke Childs <lukechilds123@gmail.com>
2020-07-11 14:56:11 +0700

    Update umbrel-dev to point to getumbrel org

--- umbrel-dev.rb
+++ umbrel-dev.rb
@@ -1,7 +1,7 @@
 class UmbrelDev < Formula
   desc "Automatically initialize and manage an Umbrel development environment."
-  homepage "https://github.com/lukechilds/umbrel-dev"
-  url "https://github.com/lukechilds/umbrel-dev/archive/0.1.0.zip"
+  homepage "https://github.com/getumbrel/umbrel-dev"
+  url "https://github.com/getumbrel/umbrel-dev/archive/0.1.0.zip"
   version "0.1.0"
   sha256 "034997cc2e1d115b99cb350fafa9f3d1b46f421acc9d49b9012aa072cd43da57"
 
