7bbef46ad7166f7c4687943babb05708dafc1115
Luke Childs <lukechilds123@gmail.com>
2020-10-16 14:45:09 +0700

    Update umbrel-dev to 1.0.0

--- umbrel-dev.rb
+++ umbrel-dev.rb
@@ -1,12 +1,12 @@
 class UmbrelDev < Formula
   desc "Automatically initialize and manage an Umbrel development environment."
   homepage "https://github.com/getumbrel/umbrel-dev"
-  url "https://github.com/getumbrel/umbrel-dev/archive/0.1.0.zip"
-  version "0.1.0"
-  sha256 "034997cc2e1d115b99cb350fafa9f3d1b46f421acc9d49b9012aa072cd43da57"
+  url "https://github.com/getumbrel/umbrel-dev/archive/1.0.0.zip"
+  version "1.0.0"
+  sha256 "8c48edd6d88ac27eea73b790424a4a7edacd25f18e7a289816751b7f5f6b51ae"
 
   def install
-    prefix.install "umbrel-dev", "Vagrantfile"
+    prefix.install "umbrel-dev", "Vagrantfile", "docker-compose.override.yml"
     bin.install "bin/umbrel-dev"
   end
 end
